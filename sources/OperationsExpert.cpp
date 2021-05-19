7c124d7f6fd045286e1cdddfcea0a5bbec900163
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 14:02:00 +0300

    Update OperationsExpert.cpp

--- sources/OperationsExpert.cpp
+++ sources/OperationsExpert.cpp
@@ -1,11 +1,11 @@
+#include <iostream>
 #include "OperationsExpert.hpp"
-
 
 namespace pandemic
 {
   
-    OperationsExpert::OperationsExpert(Board board,City city)
-    {
+  OperationsExpert::OperationsExpert(pandemic::Board& board , pandemic::City city):Player(board , city)
+  {
         
     }
 
