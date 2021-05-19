f8659764bfaebb53dd0f52ac2b323676cf4f2cde
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 14:26:25 +0300

    Add files via upload

---
+++ sources/Scientist.cpp
@@ -0,0 +1,11 @@
+#include "Scientist.hpp"
+
+
+namespace pandemic
+{
+    Scientist::Scientist(Board board, City city,int d_1)
+    {
+
+    }
+    
+}
---
+++ sources/Scientist.hpp
@@ -0,0 +1,13 @@
+#include "Player.hpp"
+
+
+namespace pandemic
+{
+    class Scientist : public Player
+    {
+     
+        public:
+        Scientist(Board board, City city,int d_1);
+
+    };
+}
