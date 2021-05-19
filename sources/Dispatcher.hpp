41524500a3391ca4174b22372ce5466148f24693
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 13:37:23 +0300

    Update Dispatcher.hpp

--- sources/Dispatcher.hpp
+++ sources/Dispatcher.hpp
@@ -1,11 +1,32 @@
+#pragma once
+#include <iostream>
 #include "Player.hpp"
+
 
 namespace pandemic
 {
     class Dispatcher : public Player
     {
         public:
-        Dispatcher(Board board,City city);
-
-    };
+        Dispatcher(pandemic::Board& board , pandemic::City city);
+        Player& fly_direct(City _city) 
+        {
+                if(city == _city)
+                {
+                         throw std::out_of_range{"Error!! cannot fly from city to it self\n"};
+                }
+                
+                if(board.node(city).getResearch_station()==false)
+                {
+                        Player::fly_direct(_city);
+                }
+                city = _city;
+                return *this;
+        }
+        
+        std::string role()
+        {
+                return "Dispatcher";
+        }
+};
 }
