f5c6e4cb38f13e2f0b671636d8e75ead52d09076
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 13:38:09 +0300

    Add files via upload

---
+++ sources/FieldDoctor.cpp
@@ -0,0 +1,11 @@
+#include "FieldDoctor.hpp"
+
+
+namespace pandemic
+{
+    FieldDoctor::FieldDoctor(Board board, City city)
+    {
+
+    }
+    
+}
---
+++ sources/FieldDoctor.hpp
@@ -0,0 +1,12 @@
+#include "Player.hpp"
+
+
+namespace pandemic{
+    class FieldDoctor : public Player
+    {
+    
+            public:
+        FieldDoctor(Board board, City city);
+
+    };
+}
