179b667c9d4c2a86ed083d18f73673a52e9ed0c5
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 13:26:42 +0300

    Add files via upload

---
+++ sources/City.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+namespace pandemic
+{
+    enum class City
+    {
+        //blue/yellow/black/red city
+        Algiers,
+        Atlanta,
+        Baghdad,
+        Bangkok,
+        Beijing,
+        Bogota,
+        BuenosAires,
+        Cairo,
+        Chennai,
+        Chicago,
+        Delhi,
+        Essen,
+        HoChiMinhCity,
+        HongKong,
+        Istanbul,
+        Jakarta,
+        Johannesburg,
+        Karachi,
+        Khartoum,
+        Kinshasa,
+        Kolkata,
+        Lagos,
+        Lima,
+        London,
+        LosAngeles,
+        Madrid,
+        Manila,
+        MexicoCity,
+        Miami,
+        Milan,
+        Montreal,
+        Moscow,
+        Mumbai,
+        NewYork,
+        Osaka,
+        Paris,
+        Riyadh,
+        SanFrancisco,
+        Santiago,
+        SaoPaulo,
+        Seoul,
+        Shanghai,
+        StPetersburg,
+        Sydney,
+        Taipei,
+        Tehran,
+        Tokyo,
+        Washington
+    };
+}
