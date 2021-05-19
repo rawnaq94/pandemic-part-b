c0ea504a042f320e29a63424a8453be7ba28e5df
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 14:27:26 +0300

    Update Scientist.cpp

--- sources/Scientist.cpp
+++ sources/Scientist.cpp
@@ -1,10 +1,9 @@
 #include "Scientist.hpp"
-
+#include <iostream>
 
 namespace pandemic
 {
-    Scientist::Scientist(Board board, City city,int d_1)
-    {
+     Scientist::Scientist(pandemic::Board& board , pandemic::City city , int k):Player(board , city),n(k){
 
     }
     
