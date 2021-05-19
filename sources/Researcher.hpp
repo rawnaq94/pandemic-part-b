04edbf8cc5586ba7aeff850c5bbbf9f8f5b9f793
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 14:25:54 +0300

    Update Researcher.hpp

--- sources/Researcher.hpp
+++ sources/Researcher.hpp
@@ -1,3 +1,5 @@
+#pragma once
+#include <iostream>
 #include "Player.hpp"
 
 
@@ -7,7 +9,67 @@ namespace pandemic
     {
     
          public:
-        Researcher(Board board, City city);
+        Researcher(pandemic::Board& board , pandemic::City city);
+    Player& discover_cure(Color color)
+    {
+        int a =0;
+        if(color == Color::Black)
+        {a=0;}
+        else if(color == Color::Blue) 
+        {a=1;}
+        else if(color == Color::Red)
+        {a=2;}
+        else if(color == Color::Yellow)
+        {a=3;}
+        
+        if(board.gerColorArr(a)==false)
+        {
+            int sum = 0;
+            City cityColorArr[5];
+            std::list<pandemic::City>::iterator temp = cards.begin();
+            while (temp!=cards.end() && sum < 5) // move on 
+            {
+                if(board.node(*temp).getColor() ==  color) 
+                {
+                    cityColorArr[sum] = *temp;
+                    sum++;
+                }
+                temp++;
 
-    };
+            }
+            if(sum < 5)
+            {
+                throw std::out_of_range{"ERROR!! you dont hava enoght cards in this color of desieas\n"};
+
+            }
+            if(sum == 5)
+            {
+                temp = cards.begin();
+                sum =0 ;
+                while (sum < 5 )  // move on 
+                {
+                    temp = cards.begin();
+                    while(temp!=cards.end())
+                    {
+                        if(*temp  ==  cityColorArr[sum]) { 
+                            cards.remove(cityColorArr[sum]);
+                            sum++;
+                            break;
+                        }   
+                        temp++;
+                    }
+
+                }
+                board.gerColorArr(a) = true;
+                
+            }
+        }
+        return *this;
+    }
+    std::string role(){
+        return "Researcher";
+    }
+
+
+};
 }
