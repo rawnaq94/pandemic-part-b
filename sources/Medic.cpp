f9eb293a3c19811f604b7ad80488fbd1ab18ee7e
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 13:51:32 +0300

    Update Medic.cpp

--- sources/Medic.cpp
+++ sources/Medic.cpp
@@ -1,10 +1,11 @@
+#include <iostream>
 #include "Medic.hpp"
 
 
 namespace pandemic
 {
-     Medic::Medic(Board board,City city)
-     {
+    Medic::Medic(pandemic::Board& board , pandemic::City city):Player(board , city)
+    {  
          
      }
 }
