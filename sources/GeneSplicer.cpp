994507322707e553568670a12e1918077dce0223
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-19 13:45:00 +0300

    Update GeneSplicer.cpp

--- sources/GeneSplicer.cpp
+++ sources/GeneSplicer.cpp
@@ -1,11 +1,12 @@
+#include <iostream>
 #include "GeneSplicer.hpp"
 
 
 namespace pandemic
 {
-    GeneSplicer::GeneSplicer(Board board, City city)
+    GeneSplicer::GeneSplicer(pandemic::Board& board , pandemic::City city):Player(board , city)
     {
-
+        
     }
 
 }
